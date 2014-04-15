1215cc91b166e7804bac5495a0fc2c132027e8a4
Richard Hattersley <rhattersley@gmail.com>
2014-04-15 14:43:03 +0100

    Add udunits to site.cfg during build.

--- iris/build.sh
+++ iris/build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# Make sure iris can find the udunits library while it's in the temporary
+# build environment. This is necessary because the build process also compiles
+# the pyke rules, which requires importing iris.
+if [[ $(uname) == Darwin ]]
+then
+    EXT=dylib
+else
+    EXT=so
+fi
+SITECFG=lib/iris/etc/site.cfg
+echo "[System]" > $SITECFG
+echo "udunits2_path = $PREFIX/lib/libudunits2.${EXT}" >> $SITECFG
+
 python setup.py install
 
